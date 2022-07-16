ddd257081fcf3fd114b00a44a6e5278e2bcffec1
Greg Pyatt <greg.pyatt@gmail.com>
2022-07-15 18:53:44 -0700

    Added args handlers.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,31 @@ class Program
          
     static void Main(string[] args)
     {
+        double maxWaitTime = 0;
         // DotNet recently got rid of much of the ceremony code in console apps, but I kick it old school.
         // See https://aka.ms/new-console-template for more information
+        // Get args
+        if (args.Length == 0)
+        {
+            // Display message to user to provide parameters.
+            Console.WriteLine("Please enter maximum number of minutes between blinks.");
+            if (!double.TryParse(Console.ReadLine(), out maxWaitTime)) {
+                Console.WriteLine("There was an error while args.Length==0");
+            }
+        }else{
+            // Loop through array to list args parameters.
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write(args[i] + Environment.NewLine);
+            }
+            if (!double.TryParse(args[0], out maxWaitTime)) {
+                Console.WriteLine("There was an error while args.Length!=0");
+            }            
+        }
+        
         Console.WriteLine("Here we go, baby!");
         RandomTimeBlinks rtb = new RandomTimeBlinks();
-        rtb.SetTimers();
+        rtb.SetTimers(maxWaitTime);
     }
 }
 
@@ -36,12 +56,12 @@ public class RandomTimeBlinks
         PlugIPs.Add("Bathroom Fan", "192.168.1.5");
     }
 
-    public void SetTimers()
+    public void SetTimers(double maximumWaitTime)
     {
         bool bugSwitch = true;
         while (true)
         {
-            displayTime = (timer.NextDouble() * 1);
+            displayTime = (timer.NextDouble() * maximumWaitTime);
             //adding safety measure of 10 seconds, but should add error handlers later.
             sleepTime = (int)((displayTime * 60000) + 10000);  //60,000 is the number of miliseconds in a minute, naturally.
             bugSwitch = (bugSwitch) ? false : true;  //Let's just work with Bug 1 & 2 for now.
@@ -61,7 +81,7 @@ public class RandomTimeBlinks
                 plugName = "Bug 1";
                 break;
             case false:
-                plugName = "Bug 1";
+                plugName = "Bug 2";
                 break;
         }
         var smartPlug = await TPLinkSmartPlug.Create(PlugIPs[plugName].ToString());
